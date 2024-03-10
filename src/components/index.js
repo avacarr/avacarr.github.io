7324439dda1bfc3ccd472c7b65e62f93f0152d08
avacarr <avacarr68@gmail.com>
2024-03-10 18:40:02 -0500

    Updates

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-
-export { default as NavBar } from './NavBar/NavBar'
-export { default as Background } from './Background/Background.jsx'
-export { default as Resume } from './Resume/Resume'
-export { default as ProjectList } from './ProjectList/ProjectList'
-export { default as Signature } from './Signature/Signature'
-export { default as ContactBar } from './ContactBar/ContactBar'
--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-
-export { default as About } from './1.About/About'
-export { default as Experience } from './2.Experience/Experience'
-export { default as Contact } from './3.Contact/Contact'
-export { default as More } from './4.More/More'
-
-
